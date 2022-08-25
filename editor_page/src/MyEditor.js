3ae6d49f1ab0001c49a9746755695eb2d0434577
Dotan Di Castro <dotan.dicastro@gmail.com>
2022-08-25 16:47:05 +0300

    update

---
+++ editor_page/src/MyEditor.js
@@ -0,0 +1,68 @@
+import React, { useEffect, useState } from "react";
+import { Editor } from "react-draft-wysiwyg";
+import { EditorState } from "draft-js";
+import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
+import styled from "styled-components";
+
+function clickMe() {
+  alert("You clicked me!");
+}
+
+const theme = {
+  blue: {
+    default: "#3f51b5",
+    hover: "#283593"
+  },
+  pink: {
+    default: "#e91e63",
+    hover: "#ad1457"
+  }
+};
+
+
+const Button = styled.button`
+  background-color: ${(props) => theme[props.theme].default};
+  color: white;
+  padding: 5px 15px;
+  border-radius: 5px;
+  outline: 0;
+  text-transform: uppercase;
+  margin: 10px 0px;
+  cursor: pointer;
+  box-shadow: 0px 2px 2px lightgray;
+  transition: ease background-color 250ms;
+  &:hover {
+    background-color: ${(props) => theme[props.theme].hover};
+  }
+  &:disabled {
+    cursor: default;
+    opacity: 0.7;
+  }
+`;
+
+Button.defaultProps = {
+  theme: "blue"
+};
+
+export default function MyEditor() {
+  const [editorState, setEditorState] = useState(() =>
+    EditorState.createEmpty()
+  );
+  useEffect(() => {
+    console.log(editorState);
+  }, [editorState]);
+  return (
+    <div>
+      <h2>Editor</h2>
+      <div style={{ border: "1px solid black", padding: '2px', minHeight: '400px' }}>
+        <Editor
+          editorState={editorState}
+          onEditorStateChange={setEditorState}
+        />
+      </div>
+      <div>
+        <Button onClick={clickMe}>Save</Button>
+      </div>
+    </div>
+  );
+}
